21605bf01901a6a58353eb4ab5dc7a76c9155b36
Mona <16801528+panmona@users.noreply.github.com>
2025-11-24 15:54:23 +0100

    Update contract to support account abstraction

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,11 +2,20 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 /** 
  * @title VotingContract
  * @dev Contains multiple votings
  */
-contract VotingContract {
+contract VotingContract is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
     struct Participant {
         address adr;
         bool voted;
@@ -182,5 +191,66 @@ contract VotingContract {
     function getVoting(uint votingIdx) internal view returns (Voting storage voting) {
         // todo: if not in array, throw?
         return votings[votingIdx];
+    }
+
+    // ---
+    // Functions for Account abstraction:
+    // ---
+    // TODO we maybe need to make the other functions to make the vote internal or similar?
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
+    // Pimlico EntryPoint V7 https://docs.pimlico.io/guides/supported-chains#ethereum
+    IEntryPoint public immutable entryPoint = IEntryPoint(0x0000000071727De22E5E9d8BAf0edAc6f37da032);
+    uint256 public nonce;
+
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external override returns (uint256 validationData) {
+        require(msg.sender == address(entryPoint), "Only EntryPoint");
+        // Verify signature
+        bytes32 hash = userOpHash.toEthSignedMessageHash();
+        address signer = hash.recover(userOp.signature); //sig is just bytes, you can multisig here
+
+        if (signer != owner()) {
+            return 1; // Invalid signature
+        }
+
+        // Validate and increment nonce
+        require(nonce++ == userOp.nonce, "Invalid nonce");
+
+        // Pay EntryPoint for gas if needed
+        if (missingAccountFunds > 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds}("");
+            require(success, "Failed to pay EntryPoint");
+        }
+
+        return 0; // Signature valid
+    }
+
+    function execute(address dest, uint256 value, bytes calldata func) external {
+        require(msg.sender == address(entryPoint), "Only EntryPoint");
+        (bool success,) = dest.call{value: value}(func);
+        require(success, "Execution failed");
+    }
+
+    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
+        require(msg.sender == address(entryPoint), "Only EntryPoint");
+        require(dest.length == func.length, "Length mismatch");
+        for (uint256 i = 0; i < dest.length; i++) {
+            (bool success,) = dest[i].call(func[i]);
+            require(success, "Batch execution failed");
+        }
+    }
+
+    // Receive ETH
+    receive() external payable {}
+
+    // Withdraw ETH - only owner
+    function withdraw(address payable to, uint256 amount) external onlyOwner {
+        (bool success,) = to.call{value: amount}("");
+        require(success, "Withdraw failed");
     }
 }
