ee66ae6bd597dfa35f1b0c2f808e7245b41f4054
Mona <16801528+panmona@users.noreply.github.com>
2025-12-02 23:08:07 +0100

    Add Account Abstraction (#20)
    - Update contract and abi
    - Fully integrate account abstraction into voting client

--- client/src/utils/voting-client.ts
+++ client/src/utils/voting-client.ts
@@ -1,9 +1,10 @@
 import { decodeBytes32String, encodeBytes32String } from "ethers";
-import { Address, createPublicClient, createWalletClient, custom } from "viem";
+import { Address, createPublicClient, custom } from "viem";
 import { sepolia } from "viem/chains";
 import { Voting } from "../types/Voting";
 import abi from "./voting-abi.json";
 import { User } from "../types/User";
+import { executeSmartAccountTransaction } from "./account-abstraction";
 
 if (!window.ethereum) throw new Error("MetaMask not found");
 export const votingClient = createPublicClient({
@@ -11,47 +12,30 @@ export const votingClient = createPublicClient({
   transport: custom(window.ethereum),
 });
 
-export async function getWalletClient() {
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  return createWalletClient({
-    chain: sepolia,
-    transport: custom(window.ethereum),
-    account: accounts[0] as `0x${string}`,
-  });
-}
-
-const contractAddress = "0x8bE31824C51777A44020ec4eCd0A9702a6a725ed";
+const CONTRACT_ADDRESS = "0xc36ab91320CD82841eb58a18Ef8a4b390b0D2430";
 
 export async function createNewVoting(
   topicName: string,
   optionNames: string[],
   participantAddresses: string[]
 ) {
-  const walletClient = await getWalletClient();
   const encodedOptions = optionNames.map((opt) =>
     encodeBytes32String(opt.trim())
   );
-  await walletClient.writeContract({
-    address: contractAddress,
-    chain: sepolia,
-    abi,
-    functionName: "createVoting",
-    args: [
-      encodeBytes32String(topicName),
-      encodedOptions,
-      participantAddresses,
-    ],
-  });
+  const args = [
+    encodeBytes32String(topicName),
+    encodedOptions,
+    participantAddresses,
+  ];
+  await executeSmartAccountTransaction(CONTRACT_ADDRESS, "createVoting", args);
 }
 
 export async function loadVotings(user: User): Promise<Voting[]> {
   const data = await votingClient.readContract({
-    address: contractAddress,
+    address: CONTRACT_ADDRESS,
     abi,
     functionName: "getVotingInfos",
-    account: <Address>user.address
+    account: <Address>user.address,
   });
 
   console.log("data", data);
@@ -73,15 +57,8 @@ export async function loadVotings(user: User): Promise<Voting[]> {
 
 export async function vote(votingId: number, optionId: number) {
   try {
-    const walletClient = await getWalletClient();
-    const txHash = await walletClient.writeContract({
-      address: contractAddress,
-      chain: sepolia,
-      abi,
-      functionName: "vote",
-      args: [votingId, optionId],
-    });
-    return txHash;
+    const args = [votingId, optionId];
+    await executeSmartAccountTransaction(CONTRACT_ADDRESS, "vote", args);
   } catch (error) {
     console.error("Voting failed: ", error);
     throw error;
